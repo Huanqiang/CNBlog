086132441bdcffdb583c522c1462b1deea4914c9
王焕强 <wanghuanqiang@wanghuanqiangdeMacBook-Pro.local>
2015-05-30 11:55:03 +0800

    Initial Commit

---
+++ CNBlogsClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CNBlogsClient
+//
+//  Created by 王焕强 on 15/5/30.
+//  Copyright (c) 2015年 王焕强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
